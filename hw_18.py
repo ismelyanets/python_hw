03a6f5db7bad5bd23b062edc20ad4f31593902fe
ismelyanets <39156200+ismelyanets@users.noreply.github.com>
2018-05-25 10:09:13 +0300

    Update hw_18.py

--- hw_18.py
+++ hw_18.py
@@ -1,9 +1,8 @@
 def sum_symbol_codes(first, last):
-    for char in (first, last):
-        code_first = ord(first)
-        code_last = ord(last)
-        count_code = (code_last - code_first) + 1
-        total_sum = (code_first + code_last)*count_code / 2
+    code_first = ord(first)
+    code_last = ord(last)
+    count_code = (code_last - code_first) + 1
+    total_sum = (code_first + code_last)*count_code / 2
     return int(total_sum)
 
 print(sum_symbol_codes('x', 'z'))
