c70950fd3b3d5b8b3bbfa143211de3554c2acba5
Igor <ismelyanets@gmail.com>
2018-06-08 19:42:39 +0300

    test 1-3

---
+++ controltask_1-3.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+c = 30
+
+print('--------Task1--------------------')
+result = ((a + b*c) ** 2)
+print("Результат при a=%d, b=%d, c=%d равен %.2f" % (a, b, c, result))
+
+print('---------Task2------------------')
+result = a - (4*b / c)
+print("Результат при a=%d, b=%d, c=%d равен %.2f" % (a, b, c, result))
+
+print('---------Task3------------------')
+result = (a * b + 4) / (c - 1)
+print("Результат при a=%d, b=%d, c=%d равен %.2f" % (a, b, c, result))
+
