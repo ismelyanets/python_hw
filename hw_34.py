8aa4fdaab0c7b1bf6be8034326727927533c0b34
Igor <ismelyanets@gmail.com>
2018-07-17 15:31:58 +0300

    hw_34

--- hw_34.py
+++ hw_34.py
@@ -2,14 +2,14 @@ import random
 
 
 class Godzilla():
-    stomach_volume = 1000
 
+    def __init__(self, weight):
+        self.weight = weight
 
-    def eat(self):
-        limit_stomach = (0.9 * self.stomach_volume)
+    def eat(self, man):
+        limit_stomach = (0.9 * self.weight)
         fill_stomach_volume = 0
         while fill_stomach_volume < limit_stomach:
-            man = random.randint(30, 75)
             if (fill_stomach_volume + man) <= limit_stomach:
                 fill_stomach_volume += man
                 print('Current volume %d, man = %d' % (fill_stomach_volume, man))
@@ -17,9 +17,10 @@ class Godzilla():
                 print('I am full and can not eat people anymore')
                 break
 
+a = Godzilla(200)
 
-a = Godzilla()
+print(a.eat(random.randint(30, 75)))
 
-print(a.eat())
+
 
 
