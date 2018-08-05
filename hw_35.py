e56c410e7a961c9128d9916e6d896bd5028a578f
Igor <ismelyanets@gmail.com>
2018-08-05 14:27:46 +0300

    hw_35

--- hw_35.py
+++ hw_35.py
@@ -4,18 +4,19 @@ class Circle:
         self.b = b
         self.r = r
 
-    def include(self, x, y):
-        if (x - self.a)**2 + (y - self.b)**2 <= (self.r)**2:
-            print('This point inside the circle')
+    def include(self, d):
+        if (d.x - self.a)**2 + (d.y - self.b)**2 <= (self.r)**2:
+            return True
         else:
-            print('This point is not inside the circle')
+            return False
 
 
 class Dot:
     def __init__(self, x, y):
-        self.dot = (x, y)
+        self.x = x
+        self.y = y
 
-d = Dot(0, 5)
+d = Dot(3, 5)
 t = Circle(3, 4, 3)
-print(t.include(0, 5))
+print(t.include(d))
 
